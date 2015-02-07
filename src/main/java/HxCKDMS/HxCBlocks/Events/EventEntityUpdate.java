ebb838553de8f833b147ebd4031b84a718396616
DrZed <kyahne12@yahoo.com>
2015-02-07 17:34:51 -0600

    First Debuff for soul regen.

--- src/main/java/HxCKDMS/HxCBlocks/Events/EventEntityUpdate.java
+++ src/main/java/HxCKDMS/HxCBlocks/Events/EventEntityUpdate.java
@@ -5,6 +5,8 @@ import HxCKDMS.HxCCore.HxCCore;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.entity.living.LivingEvent;
 
 import java.io.File;
@@ -23,8 +25,11 @@ public class EventEntityUpdate {
             if (soul < 1.0f) {
                 SoulTimer--;
                 if (SoulTimer <= 0){
-                    NBTFileIO.setFloat(CustomPlayerData, "Soul", (soul+0.1f));
+                    NBTFileIO.setFloat(CustomPlayerData, "Soul", (soul + 0.1f));
                     SoulTimer = 12000;
+                }
+                if (SoulTimer == 5000) {
+                    player.addPotionEffect(new PotionEffect(Potion.wither.getId(), 10, 1, false));
                 }
             }
         }
