87e8288f05d75c1d47a5f0ff8821fb181ee5bb3d
DrZed <kyahne12@yahoo.com>
2015-05-20 14:34:47 -0400

    Fixed Barrier Block

--- src/main/java/HxCKDMS/HxCBlocks/TileEntities/TileBarrier.java
+++ src/main/java/HxCKDMS/HxCBlocks/TileEntities/TileBarrier.java
@@ -11,33 +11,12 @@ public class TileBarrier extends TileEntity{
         if (worldObj != null && !worldObj.isRemote) {
             List list  = worldObj.getEntitiesWithinAABB(Entity.class, getAreaBoundingBox(xCoord, yCoord, zCoord));
             for (Entity entity : (List<Entity>) list) {
-
-                double newMotionX;
-                if(xCoord - entity.posX > 0)
-                    newMotionX = -entity.motionX - 0.08;
-                else
-                    newMotionX = -entity.motionX + 0.08;
-
-                double newMotionZ;
-                if(zCoord - entity.posZ > 0)
-                    newMotionZ = -entity.motionZ - 0.08;
-                else
-                    newMotionZ = -entity.motionZ + 0.08;
-
-                /*System.out.println("newMotionZ = " + newMotionZ);
-                System.out.println("newMotionX = " + newMotionX);*/
-
-                /*double newMotionX = -entity.motionX + (-entity.motionX < 0 ? -0.1 : 0.1);
-                   double newMotionZ = -entity.motionZ + (-entity.motionZ < 0 ? -0.1 : 0.1);*/
-
-                entity.addVelocity(newMotionX, -entity.motionY, newMotionZ);
-
-                //entity.addVelocity(-entity.motionX - 0.07, -entity.motionY, -entity.motionZ - 0.07);
-                //TODO: Make this properly working >_> Karel
+                entity.motionX = -entity.motionX;
+                entity.motionZ = -entity.motionZ;
             }
         }
     }
     protected AxisAlignedBB getAreaBoundingBox(float x, float y, float z) {
-        return AxisAlignedBB.getBoundingBox(x, y, z, x + 1.1, y+4, z + 1.1);
+        return AxisAlignedBB.getBoundingBox(x, y, z, x + 0.99, y+2.99, z + 0.99);
     }
 }
